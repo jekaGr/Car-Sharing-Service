55e0c766f0ddb7950e5d0a7c95217656c0e535eb
jekaGr <jekagrusha@gmail.com>
2025-02-28 14:42:17 +0200

    create app

---
+++ src/test/java/com/example/carsharingservice/TestCarSharingServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.carsharingservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCarSharingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingServiceApplication.class, args);
+    }
+
+}
